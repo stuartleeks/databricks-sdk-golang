fb2df7c5589e097317dd30f10a7f51e884a4264c
Xinyun Zhou <me@xyzhou.com>
2019-05-27 13:31:43 +1000

    finished generic

--- generic/JobTask.go
+++ generic/JobTask.go
@@ -1,4 +1,8 @@
 package generic
 
 type JobTask struct {
+	NotebookTask    NotebookTask    `json:"notebook_task,omitempty"`
+	SparkJarTask    SparkJarTask    `json:"spark_jar_task,omitempty"`
+	SparkPythonTask SparkPythonTask `json:"spark_python_task,omitempty"`
+	SparkSubmitTask SparkSubmitTask `json:"spark_submit_task,omitempty"`
 }
