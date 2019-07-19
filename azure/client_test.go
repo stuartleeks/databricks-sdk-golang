1782c690f015803340543b6f7f4d76272cbaab90
Jacob Zhou <jacob.zhou@dataexchange.work>
2019-07-19 14:12:11 +1000

    adding proper ginkgo tests

---
+++ azure/client_test.go
@@ -0,0 +1,59 @@
+package azure_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	databricks "github.com/xinsnake/databricks-sdk-golang"
+	. "github.com/xinsnake/databricks-sdk-golang/azure"
+)
+
+var _ = Describe("Client", func() {
+	testUser := "test-user"
+	option := databricks.DBClientOption{
+		User: testUser,
+	}
+
+	var dbClient DBClient
+	dbClient.Init(option)
+
+	It("Should init with correct option", func() {
+		Expect(dbClient.Option.User).To(Equal(testUser))
+	})
+
+	It("Should return initialised ClustersAPI", func() {
+		Expect(dbClient.Clusters().Client.Option.User).To(Equal(testUser))
+	})
+
+	It("Should return initialised DbfsAPI", func() {
+		Expect(dbClient.Dbfs().Client.Option.User).To(Equal(testUser))
+	})
+
+	It("Should return initialised GroupsAPI", func() {
+		Expect(dbClient.Groups().Client.Option.User).To(Equal(testUser))
+	})
+
+	It("Should return initialised JobsAPI", func() {
+		Expect(dbClient.Jobs().Client.Option.User).To(Equal(testUser))
+	})
+
+	It("Should return initialised LibrariesAPI", func() {
+		Expect(dbClient.Libraries().Client.Option.User).To(Equal(testUser))
+	})
+
+	It("Should return initialised ScimAPI", func() {
+		Expect(dbClient.Scim().Client.Option.User).To(Equal(testUser))
+	})
+
+	It("Should return initialised SecretsAPI", func() {
+		Expect(dbClient.Secrets().Client.Option.User).To(Equal(testUser))
+	})
+
+	It("Should return initialised TokenAPI", func() {
+		Expect(dbClient.Token().Client.Option.User).To(Equal(testUser))
+	})
+
+	It("Should return initialised WorkspaceAPI", func() {
+		Expect(dbClient.Workspace().Client.Option.User).To(Equal(testUser))
+	})
+})
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.12
 
 require (
 	github.com/google/go-querystring v1.0.0
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
 	github.com/spf13/pflag v1.0.3 // indirect
 	golang.org/x/tools v0.0.0-20190709211700-7b25e351ac0e // indirect
 	k8s.io/gengo v0.0.0-20190327210449-e17681d19d3a // indirect
